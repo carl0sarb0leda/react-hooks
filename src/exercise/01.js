deee82aef72dc44d96a9e5187f14921696d47cc7
carl0sarb0leda <carlosaepn@gmail.com>
2021-08-08 17:56:11 +1200

    Complete exercise 01 epic react hooks

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -3,19 +3,22 @@
 
 import * as React from 'react'
 
-function Greeting() {
+function Greeting( {initialName = ''} ) { //Do not forget to initialize the prop in case is not passed a value
   // 💣 delete this variable declaration and replace it with a React.useState call
-  const name = ''
+  // const name = ''
+  const [name, setName] = React.useState(initialName)
 
   function handleChange(event) {
     // 🐨 update the name here based on event.target.value
+    let input = event.target.value
+    setName(input ? input : null)
   }
 
   return (
     <div>
       <form>
         <label htmlFor="name">Name: </label>
-        <input onChange={handleChange} id="name" />
+        <input value={name} onChange={handleChange} id="name" />
       </form>
       {name ? <strong>Hello {name}</strong> : 'Please type your name'}
     </div>
@@ -23,7 +26,7 @@ function Greeting() {
 }
 
 function App() {
-  return <Greeting />
+  return <Greeting initialName="Charlie Brown"/>
 }
 
 export default App
